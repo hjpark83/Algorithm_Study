4b7c35e1ee956fe19b0ded9fc797c94dba45a8db
Hyunjoon83 <junippini83@hanyang.ac.kr>
2024-03-13 20:43:05 +0900

    10주차 스터디

---
+++ 10주차 스터디/NoIntersect.py
@@ -0,0 +1,28 @@
+# Question: BJ 20922 (https://www.acmicpc.net/problem/20922)
+# Rank: Silver 1
+# Algorithm: Two Pointer
+
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+# 각 숫자가 나타난 횟수를 저장하는 딕셔너리
+count = {}
+left, right = 0, 0
+max_length = 0
+
+while right < N:
+    num = A[right]
+    count[num] = count[num] + 1 if num in count else 1
+    
+    # 현재 창의 길이가 K를 초과하면 왼쪽 포인터를 이동시킴
+    while count[num] > K:
+        count[A[left]] -= 1
+        left += 1
+    
+    max_length = max(max_length, right - left + 1)
+    right += 1
+
+print(max_length)
